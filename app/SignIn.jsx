ee1064049377da41b342940f87ee67c0d41975a0
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-16 00:24:31 +0500

    Add SignIn and SignUp Pages

--- app/SignIn.jsx
+++ app/SignIn.jsx
@@ -1,11 +1,30 @@
-import { View, Text } from "react-native";
-import { useTailwind } from 'tailwind-rn'
+import { View, Text, TextInput } from "react-native";
+import { useTailwind } from 'tailwind-rn';
+import PrimaryButton from "../components/PrimaryButton";
 
 export default function SignIn() {
   const tw = useTailwind();
   return (
-    <View style={tw('flex flex-1 items-center justify-center')}>
-      <Text style={tw('text-3xl text-red-500')}>SignIn Text 123</Text>
+    <View style={tw('h-4/6 flex-col justify-between')}>
+      <View>
+        <Text style={tw('text-black text-base font-bold mt-10 text-center')}>Sign In</Text>
+      </View>
+      <View>
+        <Text style={tw('text-5xl text-indigo-500 font-bold text-left ml-5 pb-2')}>PHS</Text>
+        <Text style={tw('text-4xl text-gray-800 font-bold mt-7 text-left ml-5')}>Welcome<br/>back!</Text>
+      </View>
+      <View style={tw('flex items-center')}>
+      <View style={tw('w-4/5 rounded-2xl bg-gray-100 my-3')}>
+        <TextInput label="CNIC No" style={tw('w-auto px-5 py-5 rounded-2xl')} placeholder="3xxx-xxxxxxx-x" />
+      </View>
+      <View style={tw('w-4/5 rounded-2xl bg-gray-100 my-3 mb-5')}>
+        <TextInput style={tw('w-auto px-5 py-5 rounded-2xl')} placeholder="emily@mail.com" />
+      </View>
+      <PrimaryButton buttonText="Sign In" />
+      <View style={tw('mt-5')}>
+        <Text style={tw('text-black font-bold')}>Forgot Password?</Text>
+      </View>
+      </View>
     </View>
   );
 }
