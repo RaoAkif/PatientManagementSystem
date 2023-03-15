0573a1bb9c40ea20e594e7aec469ff27f7603482
Umman Waseem <ummanw@yahoo.com>
2023-03-15 21:17:18 +0500

    add fonts

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,13 @@
 import {TailwindProvider} from 'tailwind-rn';
 import utilities from '../tailwind.json';
 import Onboarding from "./Onboarding";
+import SignIn from './SignIn';
 
 export default function Page() {
   return (
     <TailwindProvider utilities={utilities}>
-     <Onboarding />
+     {/* <Onboarding /> */}
+     <SignIn />
    </TailwindProvider>
     
   );
