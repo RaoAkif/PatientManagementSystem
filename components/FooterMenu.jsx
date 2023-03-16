2558e7b1ae5565c66c4df07f001b455d4bc2dd14
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-16 12:50:08 +0500

    Add footer Icon highlight

--- components/FooterMenu.jsx
+++ components/FooterMenu.jsx
@@ -5,11 +5,11 @@ import { useTailwind } from 'tailwind-rn';
 export default function FooterMenu() {
   const tw = useTailwind();
   return (
-    <View style={tw('flex flex-row justify-between m-5')}>
-      <Image style={tw('w-7 h-7')} source={require('../assets/images/home.png')}></Image>
-      <Image style={tw('w-7 h-7')} source={require('../assets/images/calender.png')}></Image>
-      <Image style={tw('w-7 h-7')} source={require('../assets/images/mail.png')}></Image>
-      <Image style={tw('w-7 h-7')} source={require('../assets/images/setting.png')}></Image>
+    <View style={tw('flex flex-row justify-between m-5 items-center')}>
+      <View style={tw('bg-violet-100 p-2 rounded-2xl')}><Image style={tw('w-7 h-7')} source={require('../assets/images/home.png')} /></View>
+      <Image style={tw('w-7 h-7')} source={require('../assets/images/calender.png')} />
+      <Image style={tw('w-7 h-7')} source={require('../assets/images/mail.png')} />
+      <Image style={tw('w-7 h-7')} source={require('../assets/images/setting.png')} />
     </View>
   )
 }
