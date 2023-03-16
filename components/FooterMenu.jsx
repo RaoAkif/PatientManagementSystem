054909f4282477060cdbae0a72668f20394c5698
Rao Muhammad Akif Tufail <akifrao@gmail.com>
2023-03-16 13:43:58 +0500

    Add Settings page

--- components/FooterMenu.jsx
+++ components/FooterMenu.jsx
@@ -6,7 +6,7 @@ export default function FooterMenu() {
   const tw = useTailwind();
   return (
     <View style={tw('flex flex-row justify-between m-5 items-center')}>
-      <View style={tw('bg-violet-100 p-2 rounded-2xl')}><Image style={tw('w-7 h-7')} source={require('../assets/images/home.png')} /></View>
+      <View style={tw('bg-white p-2 rounded-2xl')}><Image style={tw('w-7 h-7')} source={require('../assets/images/home.png')} /></View>
       <Image style={tw('w-7 h-7')} source={require('../assets/images/calender.png')} />
       <Image style={tw('w-7 h-7')} source={require('../assets/images/mail.png')} />
       <Image style={tw('w-7 h-7')} source={require('../assets/images/setting.png')} />
